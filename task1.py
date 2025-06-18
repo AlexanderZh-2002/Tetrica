6d4a78ce5683e3f988630b5a507f1e3e0840a2c0
AlexanderZh-2002 <alexander.zh.2002@gmail.com>
2025-06-18 16:13:06 +0300

    Add files via upload
    better testing

--- task1.py
+++ task1.py
@@ -2,29 +2,44 @@ import inspect
 
 def strict(func):
     spec = inspect.getfullargspec(func)
-    print(spec)
     def wrapper(*args, **kwargs):
         for i in range(len(args)):
-            print("i = ", i, "name = ", spec.args[i],
-                  "type = ", spec.annotations[spec.args[i]])
             if (type(args[i]) != spec.annotations[spec.args[i]]):
                 raise TypeError
         return func(*args, **kwargs)
     return wrapper
 
 
+
 @strict
 def sum_two(a: int, b: int) -> int:
     return a + b
-
-
-print(sum_two(1, 2))  # >>> 3
-"""print(sum_two(1, 2.4))  # >>> TypeError"""
-
 
 @strict
 def fun(a: bool, b: int, c: float, d:str) -> int:
     return 1
 
-print(fun(False, 1, 1.1, "1")) # >>> 1
-print(fun(False, 1, 1.1, 1)) # >>> TypeError
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(sum_two(1, 2), 3)
+    def test_2(self):
+        with self.assertRaises(TypeError):
+            sum_two(1, 2.4)
+    def test_3(self):
+        self.assertEqual(fun(False, 1, 1.1, "1"), 1)
+    def test_4(self):
+        with self.assertRaises(TypeError):
+            print(fun(False, 1, 1.1, 1))
+    def test_4(self):
+        with self.assertRaises(TypeError):
+            print(fun(False, 1.0, 1.1, "1"))
+    def test_4(self):
+        with self.assertRaises(TypeError):
+            print(fun(False, 1, 1.1, False))
+    
+    
+if __name__ == '__main__':
+    unittest.main()
